e02d2e178b14939271f306f1e80b74dc567881e7
keelbeelveel <k.belveal17@gmail.com>
2022-05-02 15:08:24 -0400

    maked da keybinds and da vectors work

--- hrv/KeyBinds.cs
+++ hrv/KeyBinds.cs
@@ -1,5 +1,6 @@
 using Raylib_cs;
 using System;
+using System.Numerics;
 using static Raylib_cs.Raylib;
 using static Raylib_cs.KeyboardKey;
 
@@ -7,6 +8,25 @@ namespace hrv.Keybinds {
     public static class HRVKeybinds {
         public static bool Control_W() {
             return IsKeyPressed(KEY_W) && (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL));
+        }
+        public static Vector2 InputVector() {
+            Vector2 vec = new Vector2(0, 0);
+            if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) {
+                vec = vec + new Vector2(0, -1);
+            }
+            if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) {
+                vec = vec + new Vector2(0, 1);
+            }
+            if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
+                vec = vec + new Vector2(1, 0);
+            }
+            if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
+                vec = vec + new Vector2(-1, 0);
+            }
+            if (vec.Length() == 0) {
+                return vec;
+            }
+            return Vector2.Normalize(vec);
         }
     }
 }
--- hrv/Program.cs
+++ hrv/Program.cs
@@ -1,8 +1,10 @@
 using Raylib_cs;
+using static Raylib_cs.Raylib;
 using hrv.ShellWrapper;
 using hrv.Keybinds;
 using hrv.Generator;
 using System;
+using System.Numerics;
 
 namespace HelloWorld
 {
@@ -12,20 +14,44 @@ namespace HelloWorld
         {
             Raylib.InitWindow(800, 480, "HRV");
 
-            LCG lcg = new LCG();
+            LCG lcg = new LCG((uint)new Random().Next());
             Color bg = lcg.GenColor();
+
             Raylib.SetExitKey(0);
+            SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
+
+            Vector2 pos = new Vector2(12, 12);
+            int size = 20;
+            Console.WriteLine(pos.X + " " + pos.Y);
             while (!Raylib.WindowShouldClose())
             {
                 if(HRVKeybinds.Control_W())
                 {
                     Raylib.CloseWindow();
                 }
+                if(IsKeyPressed(KeyboardKey.KEY_SPACE))
+                {
+                    bg = lcg.GenColor();
+                }
+                if(IsKeyPressed(KeyboardKey.KEY_KP_ADD) || IsKeyPressed(KeyboardKey.KEY_EQUAL))
+                {
+                    size++;
+                }
+                if(IsKeyPressed(KeyboardKey.KEY_KP_SUBTRACT) || IsKeyPressed(KeyboardKey.KEY_MINUS))
+                {
+                    size--;
+                    if(size < 1)
+                    {
+                        size = 1;
+                    }
+                }
+                pos = pos + HRVKeybinds.InputVector();
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(bg);
 
-                Raylib.DrawText($"Hello, {string.Join("\n", new ShellProcess("whoami").getLines().ToArray())}", 12, 12, 20, Color.GREEN);
-                Raylib.DrawText($"{lcg.Generate(10)}", 12, 32, 20, lcg.GenColor());
+                Raylib.DrawText($"Hello, {string.Join("\n", new ShellProcess("whoami").getLines().ToArray())}", (int)pos.X, (int)pos.Y, (size), Color.GREEN);
+                Raylib.DrawText($"{lcg.Generate(10)}", (int)pos.X, (int)pos.Y+(size), size, lcg.GenColor());
+                Raylib.DrawFPS((int)pos.X, (int)pos.Y+(2*size));
 
                 Raylib.EndDrawing();
             }

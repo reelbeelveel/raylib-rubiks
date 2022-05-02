30015b56404bca4d16ecca2a4a55123cba413c79
keelbeelveel <k.belveal17@gmail.com>
2022-05-01 20:47:15 -0400

    working raylib shite and shellwrapper

--- hrv/Program.cs
+++ hrv/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Raylib_cs;
+using hrv.ShellWrapper;
+
+namespace HelloWorld
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText($"Hello, {string.Join("\n", new ShellProcess("whoami").getLines().ToArray())}", 12, 12, 20, Color.BLACK);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
---
+++ hrv/ShellWrapper.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace hrv.ShellWrapper {
+
+    class ShellException : Exception {
+        public ShellException(string message) : base(message) { }
+    }
+
+    class ShellProcess {
+
+        enum ShellProcessState {
+            Idle,
+            Started,
+            Finished
+        }
+
+        public ShellProcess(string command, string? args = null, string? redirect = null) {
+            this.command = command;
+            this._args = args;
+            this.redirect = redirect;
+            this.procInfo = new ProcessStartInfo {
+                FileName = command,
+                Arguments = this.args,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true
+            };
+            this.state = ShellProcessState.Idle;
+        }
+
+        public bool IsRunning {
+            get {
+                return state == ShellProcessState.Started;
+            }
+        }
+
+        public bool IsFinished {
+            get {
+                return state == ShellProcessState.Finished;
+            }
+            set {
+                if (value && !IsFinished) {
+                    kill();
+                    state = ShellProcessState.Finished;
+                } else if(!value && IsFinished) {
+                    throw new ShellException("Process is already finished");
+                }
+            }
+        }
+
+        public string? getLine() {
+            if(state == ShellProcessState.Finished) {
+                return null;
+            }
+            if(state == ShellProcessState.Idle) {
+                this.start();
+            }
+            if(this.proc.StandardOutput.EndOfStream) {
+                this.state = ShellProcessState.Finished;
+                return null;
+            }
+            string line = this.proc.StandardOutput.ReadLine();
+            if(proc.StandardOutput.EndOfStream) {
+                this.state = ShellProcessState.Finished;
+            }
+            using StreamWriter sw = new StreamWriter(redirect, append: true);
+            sw.WriteLine(line);
+            return line;
+        }
+
+        public void _continue() {
+            getLines();
+        }
+
+        public void _run() {
+            start();
+            getLines();
+        }
+
+        public List<string> getLines() {
+            List<string> lines = new List<string>();
+            string? line;
+            while((line = getLine()) != null) {
+                lines.Add(line);
+            }
+            return lines;
+        }
+
+        public void restart() {
+            if(state != ShellProcessState.Idle)
+                proc.Kill();
+            start();
+        }
+
+        private void kill() {
+            if(state == ShellProcessState.Idle) {
+                throw new ShellException("Process is not running");
+            }
+            proc.Kill();
+            state = ShellProcessState.Idle;
+        }
+
+        private void start() {
+            if(state == ShellProcessState.Started) {
+                throw new ShellException("Process already started");
+            }
+            proc = new Process{StartInfo = this.procInfo};
+            proc.Start();
+            state = ShellProcessState.Started;
+        }
+
+
+        public string args {
+            get {
+                return _args ?? "";
+            }
+            set {
+                if(state != ShellProcessState.Idle) {
+                    throw new ShellException("Process is already running");
+                }
+                _args = value;
+                procInfo.Arguments = value;
+            }
+        }
+
+        public string redirect {
+            get {
+                if(_redirect == null) {
+                    _redirect = $"/tmp/{Guid.NewGuid().ToString()}";
+                }
+                return _redirect;
+            }
+            set {
+                if(value == null) {
+                    _redirect = null;
+                } else {
+                    _redirect = $"/tmp/{value}";
+                }
+            }
+        }
+
+        private string command;
+        private string? _args;
+        private string? _redirect;
+        private Process? proc;
+        private ProcessStartInfo procInfo;
+        private ShellProcessState state;
+    }
+}

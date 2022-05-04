12a11681e605f012e85c366a31931bd6a9b6ac66
keelbeelveel <k.belveal17@gmail.com>
2022-05-04 16:45:10 -0400

    fixed DRI_PRIME exports to detect my setup

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
-export __NV_PRIME_RENDER_OFFLOAD=1
-export __GLX_VENDOR_LIBRARY_NAME=nvidia
-export __VK_LAYER_NV_optimus=NVIDIA_only
-export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
+#!/bin/bash
+if [[ `hostname` = "BladeStealth" ]]; then
+    if [[ `glxinfo | grep OpenGL | grep NVIDIA` = "" ]]; then
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
+    fi
+fi
 dotnet run

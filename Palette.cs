6c831f75cc1201c12fe69add40e58e9ac1d738f4
keelbeelveel <k.belveal17@gmail.com>
2022-05-04 18:40:32 -0400

    begin work on canvas

---
+++ Palette.cs
@@ -0,0 +1,9 @@
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace hrv {
+    public static class Palette {
+        public static color bg { get; set; }
+        public static color fg { get; set; } = new color(255, 255, 255, 255);
+    }
+}

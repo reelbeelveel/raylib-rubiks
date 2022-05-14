43fbb7255e701a04b6ea40ed783e908092df6c9c
keelbeelveel <k.belveal17@gmail.com>
2022-05-13 21:47:52 -0400

    fixed generator ranges, allowed it to generate moves

--- Generator.cs
+++ Generator.cs
@@ -4,10 +4,10 @@ using System.Collections;
 using System.Collections.Specialized;
 
 namespace rbx.Generator {
-    class GeneratorRangeException : Exception {
+    public class GeneratorRangeException : Exception {
         public GeneratorRangeException(string message) : base(message) { }
     }
-    class HRVGenerator {
+    public class HRVGenerator {
         public HRVGenerator(uint? seed = null) {
             this.seed = seed ?? (uint)DateTime.Now.Ticks;
             Console.WriteLine("Seed: " + this.seed);
@@ -22,20 +22,23 @@ namespace rbx.Generator {
             }
             return InternalGenerate(range);
         }
-        public byte GenByte(uint range = 255) {
-            if (range > 255) {
-                throw new GeneratorRangeException("Range must be less than 256");
+        public byte GenByte(uint range = 256) {
+            if (range > 256) {
+                throw new GeneratorRangeException("Range must be less than or equal to 256");
             }
             return (byte)this.Generate(range);
         }
-        public Color GenColor(uint range = 255, uint opacity = 255) {
-            if (range > 255) {
-                throw new GeneratorRangeException("Range must be less than 256");
+        public Color GenColor(uint range = 256, uint opacity = 255) {
+            if (range > 256) {
+                throw new GeneratorRangeException("Range must be less than or equal to 256");
             }
             if (opacity > 255) {
                 throw new GeneratorRangeException("Opacity must be less than 256");
             }
             return new Color(GenByte(range), GenByte(range), GenByte(range), (byte)opacity);
+        }
+        public rbx.Puzzle.Mvmt GenMvmt(bool noSlices = false) {
+            return (rbx.Puzzle.Mvmt)this.Generate((uint)(noSlices ? 18 : 24));
         }
         public uint seed;
         protected uint state;

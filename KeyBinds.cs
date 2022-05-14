737f9675b4f1a64ae0c862c7e7bf80cfb495226a
keelbeelveel <k.belveal17@gmail.com>
2022-05-13 21:48:36 -0400

    input movement, undo key, shuffle key

--- KeyBinds.cs
+++ KeyBinds.cs
@@ -3,6 +3,7 @@ using System;
 using System.Numerics;
 using static Raylib_cs.Raylib;
 using static Raylib_cs.KeyboardKey;
+using rbx.Puzzle;
 
 namespace rbx{
     public static class Keybinds {
@@ -18,6 +19,12 @@ namespace rbx{
         public static bool FullScreenKey() {
             return IsKeyPressed(KEY_F11);
         }
+        public static bool UndoKey() {
+            return IsKeyDown(KEY_Z) && (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL));
+        }
+        public static bool ShuffleKey() {
+            return IsKeyPressed(KEY_SPACE);
+        }
         public static bool Control_Mod(KeyboardKey key) {
             return IsKeyPressed(key) && (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL));
         }
@@ -26,6 +33,19 @@ namespace rbx{
         }
         public static bool Alt_Mod(KeyboardKey key) {
             return IsKeyPressed(key) && (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT));
+        }
+        public static Mvmt? InputMvmt() {
+            KeyboardKey[] MvmtKeys = new KeyboardKey[]{
+               KEY_X, KEY_Y, KEY_Z, KEY_F, KEY_R, KEY_U, KEY_D, KEY_B, KEY_L, KEY_M, KEY_S, KEY_E
+            };
+            for(int i = 0; i < 12; i++) {
+                if(IsKeyPressed(MvmtKeys[i])) {
+                    if(IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT))
+                        return (Mvmt)(i * 2) + 1;
+                    return (Mvmt)(i * 2);
+                }
+            }
+            return null;
         }
         public static Vector2 InputVector() {
             Vector2 vec = new Vector2(0, 0);
